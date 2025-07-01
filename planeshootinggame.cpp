0f568b2231f792b5fc45585c6beadbaf4a4f12ad
ghxy-TDK <91738206+ghxy-TDK@users.noreply.github.com>
2025-07-01 09:12:54 +0800

    Add BatchDraw

--- planeshootinggame.cpp
+++ planeshootinggame.cpp
@@ -5,9 +5,10 @@ int main() {
     srand((unsigned)time(NULL));
     initgraph(WINDOW_WIDTH, WINDOW_HEIGHT);
     setbkcolor(RGB(0, 0, 0));
+    BeginBatchDraw();
     GameManager game;
     game.run();
-
+    EndBatchDraw();
     closegraph();
     return 0;
 }
